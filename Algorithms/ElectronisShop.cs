1f97a077bdc179d8a68fce1c11417f814d7c80c7
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-06-09 18:29:25 +0545

    Project Loaded

---
+++ Algorithms/ElectronisShop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Algorithms
+{
+   public class ElectronisShop
+    {
+      public  static int getMoneySpent(int[] keyboards, int[] drives, int b)
+        {
+            /*
+             * Write your code here.
+             */
+            int max = 0;
+            for(int i=0;i<keyboards.Length;i++)
+            {
+                for(int j=0;j<drives.Length;j++)
+                {
+                    int sum = keyboards[i] + drives[j];
+                    if(sum<b)
+                    {
+                        if(sum>max)
+                        {
+                            max = sum;
+                        }
+                    }
+                }
+            }//end for loop
+            return max == 0 ? -1 : max;
+
+
+        }
+    }
+}
---
+++ Algorithms/SalesByMatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Algorithms
+{
+   public class SalesByMatch
+    {
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int counter=0;
+
+            Dictionary<int, int> values = new Dictionary<int, int>();
+            foreach(int a in ar)
+            {
+                values[a] = values.ContainsKey(a) ? values[a] + 1 : 1;
+
+            }
+            foreach(KeyValuePair<int,int> item in values)
+            {
+                counter = item.Value % 2 == 0 ? counter + (item.Value/2) : counter + (item.Value - 1)/2;
+            }
+            return counter;
+        }
+    }
+}
