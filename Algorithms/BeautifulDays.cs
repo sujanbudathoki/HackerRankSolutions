f52b15444c059c93344db29dbc310ad5004f7f2c
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-07-01 09:13:17 +0545

    Added Problem Beautiful Days

---
+++ Algorithms/BeautifulDays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.Algorithms
+{
+   public class BeautifulDays
+    {
+        public static int beautifulDays(int i, int j, int k)
+        {
+            int counter = 0;
+           
+
+            for (int l= i; l <= j; l++)
+            {
+                int result = (l - reverser(l)) % k;
+                if (result == 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+
+        }
+        private static int reverser(int n)
+        {
+            int rem = 0; int reverse = 0;
+        
+            
+            while(n>0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            return reverse;
+        }
+
+    }
+}
