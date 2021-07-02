c0e60f23385794c7526c7b45801b4e918a60947e
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-07-02 20:50:13 +0545

    added problem time conversion

---
+++ Algorithms/TimeConversion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+  public  class TimeConversion
+    {
+        public static string timeConversionFunction(string s)
+        {
+            
+            string[] times;
+            times = s.Split(':');
+            int hour = int.Parse(times[0]);
+            string minutes = times[1];
+            string seconds = times[2].Substring(0, 2);
+            string symbol = times[2].Substring(2, 2);
+            if (symbol == "AM" && hour == 12)
+            {
+                hour = 0;
+            }
+            if(symbol=="PM" && hour < 12)
+            {
+                hour += 12;
+            }
+
+
+            return hour < 10 ? $"0{hour}:{minutes}:{seconds}":$"{hour}:{minutes}:{seconds}";
+
+            
+        }
+    }
+}
