c4d3a5a55f88307f1e74459e7645b45de7e59a62
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-07-09 08:55:47 +0545

    Picking Numbers

---
+++ Algorithms/PickingNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+  public  class PickingNumbers
+    {
+        public int maxLength(List<int> n)
+        {
+            int max = 0;
+            for(int i = 0; i < n.Count - 1; i++)
+            {
+                int currIndex = i;
+                int Counter = 0;
+                bool isRepeated = i == 0 ? false : n[i] - n[i - 1] == 0 ? true : false;
+                if (!isRepeated)
+                {
+
+                    while (true)
+                    {
+                        if (n[currIndex] - n[i] <= 1)
+                        {
+                            currIndex++;
+                            Counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if (Counter > max)
+                    {
+                        max = Counter;
+                    }
+                }
+                
+            }
+            return max;
+        }
+    }
+}
