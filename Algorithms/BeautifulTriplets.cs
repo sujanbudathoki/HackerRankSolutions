8e275ea098bb697eda3b924abf2e0463e1140493
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-07-16 08:52:13 +0545

    BeautifulTriplets

---
+++ Algorithms/BeautifulTriplets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+  public  class BeautifulTriplets
+    {
+        public static int beautifulTriplets(int d, List<int> arr)
+        {
+
+            int tripletCount = 0;
+            for(int i = 0; i < arr.Count; i++)
+            {
+                int curValue = arr[i];
+                
+                    if (arr.Contains(curValue + d))
+                    {
+                        if (arr.Contains(curValue + (d * 2)))
+                        {
+                        tripletCount++;
+                        }
+                    }
+               
+                
+            }
+            return tripletCount;
+        }
+    }
+}
---
+++ Algorithms/MagicSquare.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+   public class MagicSquare
+    {
+        public static int formingMagicSquare(List<List<int>> s)
+        {
+            int result=0;
+
+            return result;
+        }
+    }
+}
