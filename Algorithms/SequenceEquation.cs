5dd7c0c13222d93e9ed59d6094e850a8564be67b
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-08-01 08:48:57 +0545

    Solved Problem Sequence Equation

---
+++ Algorithms/SequenceEquation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+     public  class SequenceEquation
+    {
+        //using dictionary
+        public static List<int> permutationEquation(List<int> p)
+        {
+            List<int> output = new List<int>(new int[p.Count]);
+            int count = 1;
+            Dictionary<int, int> dictStore = new Dictionary<int, int>();
+            for(int i = 0; i < p.Count; i++)
+            {
+                dictStore.Add(p[i], i+1);
+            }
+            while(count<=p.Count)
+            {
+                int store = dictStore[count];
+                int result = dictStore[store];
+                output.Add(result);
+                count++;
+            }
+            return output;
+
+        }
+    }
+}
---
+++ Algorithms/SubArrayDivision.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+   public class SubArrayDivision
+    {
+        public static int birthday(List<int> s, int d, int m)
+        {
+            int result = 0;
+            for (int i = 0; i <= s.Count - m; i++)
+            {
+              
+                int sum = s.GetRange(i, m).Sum();
+                result = sum == d ? result + 1 : result;
+            }
+            return result;
+        }
+    }
+}
