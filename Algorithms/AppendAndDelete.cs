b1a48486362dcb65370c59a7651e266e217c668f
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-08-01 18:27:04 +0545

    Added Problem Append and Delete

---
+++ Algorithms/AppendAndDelete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+   public class AppendAndDelete
+    {
+        public static string appendAndDelete(string s, string t, int k)
+        {
+            int minLength = s.Length<t.Length?s.Length:t.Length;
+            int minSteps = 0;
+            for(int i = 0; i < minLength; i++)
+            {
+                if (s[i] == t[i])
+                {
+                    minSteps++;
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            minSteps = s.Length + t.Length - (2 * minSteps);
+
+            return k < minSteps ? "NO" : (k - minSteps) % 2 == 0 ? "YES" : "NO";
+
+        }
+    }
+}
