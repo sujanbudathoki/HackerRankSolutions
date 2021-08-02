64c996222ef01923edb738538719f2286b139a7a
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-08-02 08:10:06 +0545

    Added Problem Cut The Sticks

---
+++ Algorithms/CutTheSticks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+    public  class CutTheSticks
+    {
+        public static List<int> cutTheSticks(List<int> arr)
+        {
+            List<int> rem = new List<int>();
+            arr.Sort();
+            rem.Add(arr.Count);
+            int remaining=arr.Count;
+            Dictionary<int, int> storage = new Dictionary<int, int>();
+            for(int i = 0; i < arr.Count; i++)
+            {
+                if (storage.ContainsKey(arr[i]))
+                {
+                    storage[arr[i]] = storage[arr[i]] + 1;
+                }
+                else
+                {
+                    storage.Add(arr[i], 1);
+                }
+            }
+            foreach(var a in storage)
+            {
+                remaining = remaining - a.Value;
+                if (remaining > 0)
+                {
+                    rem.Add(remaining);
+                }
+            }
+            return rem;
+        }
+    }
+}
---
+++ Algorithms/JumpngClouds.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+  public  class JumpngClouds
+    {
+        public static int jumpingOnClouds(List<int> c)
+        {
+            int minimum = 0;
+            for(int i = 0; i < c.Count; i+=2)
+            {
+                if (c[i] == 1)
+                {
+                    minimum++;
+                    i--;
+
+                }
+                else
+                {
+                    minimum++;
+                }
+            }
+            return minimum;
+
+        }
+    }
+}
