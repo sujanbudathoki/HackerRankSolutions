db29f0d334b9905d8c8ed00a56bdc2ae86e4b40e
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-08-03 10:54:37 +0545

    modified taumBday

--- Algorithms/TaumBday.cs
+++ Algorithms/TaumBday.cs
@@ -9,18 +9,7 @@ public    class TaumBday
         public static long taumBday(Int64 b, Int64 w, Int64 bc, Int64 wc, Int64 z)
         {
             long result = 0;
-            if (bc > wc)
-            {
-                result = bc > (wc + z) ? (wc + z) * b + wc * w : wc * w + bc * b;
-            }
-            else if (bc < wc)
-            {
-                result = wc > (bc + z) ? (bc + z) * w + bc * b : bc * b + wc * w;
-            }
-            else
-            {
-                result = bc * b + wc * w;
-            }
+            result = bc > wc ? bc > (wc + z) ? (wc + z) * b + wc * w : wc * w + bc * b : bc < wc ? wc > (bc + z) ? (bc + z) * w + bc * b : bc * b + wc * w : bc * b + wc * w;
             return result;
         }
     }
