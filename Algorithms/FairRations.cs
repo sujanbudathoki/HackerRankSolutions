daadd9fa6d8030875ff943d69ef3e1a16140e7ca
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-08-04 17:24:39 +0545

    Added Problem FairRations and Sher lockAndSquares

---
+++ Algorithms/FairRations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+   public class FairRations
+    {
+        public static string fairRations(List<int> B)
+        {
+
+            int oddCounter = B.Where(x=>x%2!=0).Count();
+            int counter = 0;
+            if (oddCounter % 2 == 0)
+            {
+                for (int i = 0; i < B.Count; i++)
+                {
+                    if (B[i] %2!= 0)
+                    {
+                        counter += 2;
+                        B[i + 1] = B[i + 1] + 1;
+                    }
+
+                }
+                return counter.ToString();
+            }
+            else
+            {
+                return "NO";
+            }
+         
+        }
+    }
+}
---
+++ Algorithms/SherlockAndSquares.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+   public class SherlockAndSquares
+    {
+        public static int squares(int a, int b)
+        {
+            
+            
+            double result = Math.Floor(Math.Sqrt(b)) - Math.Ceiling(Math.Sqrt(b)) + 1;
+
+           
+            return (int)result;
+
+        }
+    }
+}
