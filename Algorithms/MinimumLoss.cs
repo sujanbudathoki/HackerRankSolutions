f96318270a848e63363da7e90452478fad7fe87e
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-08-29 09:06:46 +0545

    Added Financial Loss

---
+++ Algorithms/MinimumLoss.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.HackerRankSolutions.HackerRankSolutions.Algorithms
+{
+  public  class MinimumLoss
+    {
+        public static int minimumLoss(List<long> price)
+        {
+            //alert :- few test cases failed
+            long minimum = price.Max();
+            for(int i = 0; i < price.Count; i++)
+            {
+                for(int j = i + 1; j < price.Count; j++)
+                {
+                    
+                    if (price[i] > price[j]) //indicates financial loss
+                    {
+                     
+                        if ((price[i]-price[j]) < minimum)
+                        {
+                            minimum = price[i] - price[j];
+                        }
+                    
+                    }
+                }
+            }
+            return (int)minimum;
+        }
+    }
+}
